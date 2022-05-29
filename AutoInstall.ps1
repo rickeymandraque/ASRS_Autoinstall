473d577ef6ff3ecf9d9e199e12a2ba47112d959b
rickeymandraque <rickeymandraque@users.noreply.github.com>
2022-05-29 18:08:29 +0200

    Version 0.1
    Refonte du code

--- AutoInstall.ps1
+++ AutoInstall.ps1
@@ -1,53 +1,54 @@
-
-$id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-
-    $p = New-Object System.Security.Principal.WindowsPrincipal($id)
-
-    if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator))
-	{
-$ARCH = $env:PROCESSOR_ARCHITECTURE
-$Name = (Get-WmiObject -class Win32_OperatingSystem).Caption
+$IsAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+$CPUArch = (@{0="x86";1="MIPS";2="Alpha";3="PowerPC";5="ARM";6="Itanium-based systems";9="x64"})[[int](Get-WMIObject -Class Win32_Processor).Architecture]
 $ComputerBrand = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
 $ComputerModel = (Get-CimInstance -ClassName Win32_ComputerSystem).Model
-$LogDir = C:\ASRSAutoinstall\$($ComputerBrand)Logs
-$LogPath="$LogDir\DriverInstallation.log"
 
-function PSCommandPath() { return $PSCommandPath; }
+$Name = (Get-WmiObject -class Win32_OperatingSystem).Caption
+
+$FullScriptPath = "$($PSCommandPath)"
+$ScriptName = "$($PSScriptRoot)"
+$ScriptPath = "$($MyInvocation.MyCommand.Name)"
+ 
+
+
+
+if ($Name -like "*Windows 10*")
+  {
+  $OS_Vers = "Win10"
+  }
+  elseif  ($Name -like "*Windows 11*")
+  {
+  $OS_Vers = "Win11"
+  }
+  else
+  {
+  echo "OS non-reconnu"
+  }
+
+
+$Drivers_Utilities_Path = ".\$ComputerBrand\$ComputerModel\"
+$Common_Drivers = "$Drivers_Utilities_Path\Common\"
+$Third_Party = "$Drivers_Utilities_Path\Third-Party\"
+# $Post-Install = ".\Post-Install\" Not implemented
+$OS_Dir = "$($OS_Vers)_$($CPUArch)"
+$Drivers_Dir = "$Drivers_Utilities_Path\$OS_Dir\"
+$INF_Dir = "$($Drivers_Dir)\INF\"
+$App_Dir = "$Drivers_Dir\App\"
+$LogDir = "C:\ASRSAutoinstall\$($ComputerBrand)Logs"
+$LogPath ="$LogDir\DriverInstallation.log"
+$ListePilotes = $(Get-ChildItem $INF_Dir -Recurse -Filter "*.inf")
+$ListeApp = $(Get-ChildItem $App_Dir -Recurse -Filter "Setup_APP.cmd")
+$Liste3rdParty = $(Get-ChildItem $App_Dir -Recurse -Filter "*Setup.exe")
 
 if(!(Test-Path -Path $LogDir ))
 {
     New-Item -ItemType directory -Path $LogDir
 }
-if ($ARCH -like "AMD64")
-{
-$CPU_Arch = 64
-}
-else
-{
-$CPU_Arch = 32
-}
 
-if ($Name -like "*Windows 10*")
-{
-
-$OS_Vers = "Win10"
-}
-elseif  ($Name -like "*Windows 11*")
-{
-$OS_Vers = "Win11"
-}
-
-else
-{
-echo "OS non-reconnu"
-exit 1
-}
-$OS_Dir = "$ComputerBrand\$ComputerModel\$($OS_Vers)_$($CPU_Arch)"
-
-		
-$ListePilotes = Get-ChildItem ".\$OS_Dir" -Recurse -Filter "*.inf"
+write-host "Installation des pilotes"
 ForEach ($Pilote in $ListePilotes)
 { 
+echo "$(Get-Date) [Log TRACE]  pnputil /add-driver $Pilote.Name  /install" >> $LogPath
   try {
          $ PNPUtil.exe /add-driver $Pilote.FullName /install 
   }
@@ -57,16 +58,32 @@ ForEach ($Pilote in $ListePilotes)
         PNPUtil.exe /add-driver $Pilote.FullName /install 
   }
 }
+echo "$(Get-Date) [Log TRACE]  Fin installation Pilotes " >> $LogPath
+write-host "Pilote installé"
+write-host "Installation des pilotes et logiciel supplémentaire"
+echo "$(Get-Date) [Log TRACE]  installation Pilotes et logiciels 3rd party (intel) " >> $LogPath
 
-  if ($($OS_Vers)_$($CPU_Arch) -like "Win10_64")
-  {
-  & ".\$OS_Dir\Turbo Boost_Intel\SetupME.exe" -s
-  }
-  echo "vous devez redémmarrer le PC"
-  & PAUSE
-	}
-	else
-	{
-		echo "vous n'êtes pas en admin, relancer le script"
-		& pause
-	}
+ForEach ($3rdParty in $Liste3rdParty)
+{ 
+echo "$(Get-Date) [Log TRACE]  $3rdParty.name  -s " >> $LogPath
+
+  & $3rdParty.FullName -s
+}
+
+echo "$(Get-Date) [Log TRACE] Fin installation Pilotes et logiciels 3rd party (intel) " >> $LogPath
+
+echo "Fin installation Pilotes et logiciels 3rd party"
+
+echo "$(Get-Date) [Log TRACE] début installation App (Acer) "  >> $LogPath
+echo "début installation App (Acer)"
+
+
+
+ForEach ($App in $ListeApp)
+{ 
+echo "$(Get-Date) [Log TRACE]  $App.name  -s " >> $LogPath
+
+  & $App.FullName
+}
+
+echo "Vous devez redémarer"
